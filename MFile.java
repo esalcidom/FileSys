96b1c7b031cd7b514ae74f96f5dc1568336ac9c3
Emmanuel <emmanuel.salcido.maldonado@gmail.com>
2016-10-27 18:01:28 -0500

    MFile class added where set all metadata from a file

---
+++ MFile.java
@@ -0,0 +1,59 @@
+/*
+ * MFile.java
+ * 
+ * Copyright 2016 Emmanuel <emmanuel.salcido.maldonado@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class MFile{
+	
+	private String name;
+	private String type;
+	private short location;
+	private short size;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getType(){
+		return this.type;
+	}
+	
+	public void setName(String type{
+		this.type = type;
+	}
+	
+	public short getLocation(){
+		return this.location;
+	}
+	
+	public void setLocation(short location){
+		
+		this.location = location;
+	}
+	
+	
+}
+
